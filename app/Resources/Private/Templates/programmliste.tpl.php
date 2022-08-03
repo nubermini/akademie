020b5c0d0549f964058556cd89cd96325920f15a
henrikgebauer <code@henrik-gebauer.de>
2022-08-03 23:26:29 +0200

    clickable links

--- app/Resources/Private/Templates/programmliste.tpl.php
+++ app/Resources/Private/Templates/programmliste.tpl.php
@@ -1,4 +1,12 @@
-<?php if (isset($beitraege)): ?>
+<?php
+function makeUrlsClickable($html) {
+    return preg_replace_callback('/https?:\/\/\S+|www\.\S+?\.\S+/', function ($matches) {
+        $url = preg_match('/^https?:\/\//', $matches[0]) ? $matches[0] : ('https://' . $matches[0]);
+        return '<a href="' . $url . '">' . $matches[0] . '</a>';
+    }, $html);
+}
+?>
+<?php if (isset($beitraege) && count($beitraege)): ?>
     <div id="programmliste">
 <?php
     foreach ($beitraege as $b):
@@ -21,11 +29,11 @@
             <?php endif; ?>
             <div class="name"><?=implode(', ', $namen)?></div>
         </div>
-        <p><?=nl2br($beitrag->get('abstract'))?></p>
+        <p><?=nl2br(makeUrlsClickable($beitrag->get('abstract')))?></p>
 
         <?php foreach ($beitrag->get('referenten') as $referent): if (trim($referent['kurzvita'])): ?>
             <h4 class="kurzvita-titel">Zu <?=$referent['name']?></h4>
-            <p class="kurzvita"><?=nl2br($referent['kurzvita'])?></p>
+            <p class="kurzvita"><?=nl2br(makeUrlsClickable($referent['kurzvita']))?></p>
         <?php endif; endforeach; ?>
 <?php endforeach; ?>
     </div>
