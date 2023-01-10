4668554e1c1dee1369ebbbcf3a492c10f8ee2b81
henrikgebauer <code@henrik-gebauer.de>
2023-01-10 23:36:45 +0100

    historie vereinfacht

---
+++ app/Resources/Private/Templates/historie.md
@@ -0,0 +1,24 @@
+# Vergangene Mind-Akademien
+
+| Jahr | Leitthema                     | Ort      |
+|------|-------------------------------|----------|
+| 2022 | [Verbindungen](programm.php?y=2022) | Mannheim |
+| 2021 | [Wandel](programm.php?y=2021) | Mannheim |
+| 2019 | [Kopf oder Zahl](programm.php?y=2019) | Mannheim |
+| 2018 | [Norm und Abweichung](programm.php?y=2018) | Mannheim |
+| 2017 | [Experimente](programm.php?y=2017) | Köln |
+| 2016 | [Wege - Kreuzungen - Wendepunkte](programmHistorie.php?y=2016) | Mannheim |
+| 2015 | Wahrheiten und Geheimnisse | Heidelberg |
+| 2014 | Chaos und Ordnung | Würzburg |
+| 2013 | Beziehungen und Relationen | Düsseldorf |
+| 2012 | [(R)Evolutionen](programmHistorie.php?y=2012) | Würzburg |
+| 2011 | [Zukunft und Forschung](programmHistorie.php?y=2011) | Hannover |
+| 2010 | [Transformation](programmHistorie.php?y=2010) | Köln |
+| 2009 | [Freiheit und Grenzen](programmHistorie.php?y=2009) | Nürnberg |
+| 2008 | [Zeit](programmHistorie.php?y=2008) | Nürnberg |
+| 2007 | [Kreativität und Innovation](programmHistorie.php?y=2007) | Würzburg |
+| 2006 | [Jenseits des Verstands](programmHistorie.php?y=2006) | Marburg |
+| 2005 | [Sprache und Kommunikation](programmHistorie.php?y=2005) | Darmstadt |
+| 2004 | [MACHT.WISSEN.SCHAFFT.SKEPSIS](programmHistorie.php?y=2004) | Darmstadt |
+| 2003 | [Entscheidungen und Entscheider](programmHistorie.php?y=2003) | Köln |
+| 2002 | [Netzwerke](programmHistorie.php?y=2002) | Frankfurt |
--- app/html/historie.php
+++ app/html/historie.php
@@ -5,30 +5,6 @@ require_once '../lib/base.inc.php';
 
 Tpl::set('navId', 'historie');
 
-$vergangeneMAs = [
-    '2021' => ['url' => 'programm.php?y=2021', 'leitthema' => 'Wandel', 'ort' => 'Mannheim'],
-    '2019' => ['url' => 'programm.php?y=2019', 'leitthema' => 'Kopf oder Zahl', 'ort' => 'Mannheim'],
-    '2018' => ['url' => 'programm.php?y=2018', 'leitthema' => 'Norm und Abweichung', 'ort' => 'Mannheim'],
-    '2017' => ['url' => 'programm.php?y=2017', 'leitthema' => 'Experimente', 'ort' => 'Köln'],
-    '2016' => ['url' => 'programmHistorie.php?y=2016', 'leitthema' => 'Wege - Kreuzungen - Wendepunkte', 'ort' => 'Mannheim'],
-    '2015' => ['url' => '#', 'leitthema' => 'Wahrheiten und Geheimnisse', 'ort' => 'Heidelberg'],
-    '2014' => ['url' => '#', 'leitthema' => 'Chaos und Ordnung', 'ort' => 'Würzburg'],
-    '2013' => ['url' => '#', 'leitthema' => 'Beziehungen und Relationen', 'ort' => 'Düsseldorf'],
-    '2012' => ['url' => 'programmHistorie.php?y=2012', 'leitthema' => '(R)Evolutionen', 'ort' => 'Würzburg'],
-    '2011' => ['url' => 'programmHistorie.php?y=2011', 'leitthema' => 'Zukunft und Forschung', 'ort' => 'Hannover'],
-    '2010' => ['url' => 'programmHistorie.php?y=2010', 'leitthema' => 'Transformation', 'ort' => 'Köln'],
-    '2009' => ['url' => 'programmHistorie.php?y=2009', 'leitthema' => 'Freiheit und Grenzen', 'ort' => 'Nürnberg'],
-    '2008' => ['url' => 'programmHistorie.php?y=2008', 'leitthema' => 'Zeit', 'ort' => 'Nürnberg'],
-    '2007' => ['url' => 'programmHistorie.php?y=2007', 'leitthema' => 'Kreativität und Innovation', 'ort' => 'Würzburg'],
-    '2006' => ['url' => 'programmHistorie.php?y=2006', 'leitthema' => 'Jenseits des Verstands', 'ort' => 'Marburg'],
-    '2005' => ['url' => 'programmHistorie.php?y=2005', 'leitthema' => 'Sprache und Kommunikation', 'ort' => 'Darmstadt'],
-    '2004' => ['url' => 'programmHistorie.php?y=2004', 'leitthema' => 'MACHT.WISSEN.SCHAFFT.SKEPSIS', 'ort' => 'Darmstadt'],
-    '2003' => ['url' => 'programmHistorie.php?y=2003', 'leitthema' => 'Entscheidungen und Entscheider', 'ort' => 'Köln'],
-    '2002' => ['url' => 'programmHistorie.php?y=2002', 'leitthema' => 'Netzwerke', 'ort' => 'Frankfurt']
-];
-
-Tpl::set('vergangeneMAs', $vergangeneMAs);
-
 Tpl::render('historie');
 
 Tpl::submit();
