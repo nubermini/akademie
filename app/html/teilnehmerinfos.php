b265fbc28128ddeb539054ba7654b5bca7cce54e
henrikgebauer <code@henrik-gebauer.de>
2023-01-10 23:49:04 +0100

    teilnehmerinfos passwortschutz vereinfacht

--- app/html/teilnehmerinfos.php
+++ app/html/teilnehmerinfos.php
@@ -3,14 +3,11 @@ declare(strict_types=1);
 
 namespace MHN\Akademie;
 
-// fest kodiert, kein sichererer Schutz erforderlich, darf auch auf github zu sehen sein, salt: )8cWa*
-const PASSWORD_HASH = '65068a59c812678e84d8349e28d546d864009f566eb6f10a55c2e1ac59c96647';
-
 require_once '../lib/base.inc.php';
 
 ensure($_POST['password'], ENSURE_STRING);
 
-if (hash('sha256', ')8cWa*' . $_POST['password']) !== PASSWORD_HASH) {
+if ($_POST['password'] !== getenv('TEILNEHMERINFOS_PASSWORD')) {
     if ($_POST['password']) {
         Tpl::set('wrongPassword', true);
     }
